18609a6401b0c36a7815abb8b5ca0ef80994189b
Ayumi <ayuayuayuayus@gmail.com>
2022-07-02 12:00:33 +0200

    02.07 done

--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 class SampleForm(FlaskForm):
-    userName = StringField('Your name', validators=[DataRequired(), Length(min=1, max=80)])
+    userName = StringField('Your name', validators=[DataRequired(), Length(min=1, max=4)])
+    userAge = IntegerField('Your Age', validators=[DataRequired()])
 
     submit = SubmitField('Submit')
